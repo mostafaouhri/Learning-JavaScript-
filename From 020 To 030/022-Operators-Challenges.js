d54884413bb8aad797b53f1d7b967221e9f8607c
Mostafa Ouhri <ouhrimostafa@gmail.com>
2025-06-08 22:07:27 +0000

    Still Learning

---
+++ From 020 To 030/022-Operators-Challenges.js
@@ -0,0 +1,19 @@
+// Challenge 1 //
+var a = 10;
+var b = "20";
+var c = 80;
+
+console.log( ++a + +b++ + +c++ - +a++ );
+console.log( ++a + -b + +c++ - -a++ + +a );
+console.log( --c + +b + --a * +b++ - +b * a + --a - +true);
+
+// Challenge 2 //
+var d = "-100";
+var e = "20";
+var f = 30;
+var g = true;
+
+console.log(-d * e); //2000
+console.log( ++f + ++e * ++g + -d); //173
+
+

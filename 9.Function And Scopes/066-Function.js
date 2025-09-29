4380d9214ba4a305e7147762afc51a8039fa7b24
Mostafa Ouhri <ouhrimostafa@gmail.com>
2025-09-29 20:54:51 +0000

    Learning

--- 9.Function And Scopes/066-Function.js
+++ 9.Function And Scopes/066-Function.js
@@ -9,6 +9,14 @@ console.log('==================');
 //     return 10;
 // };
 
-let print = () => 10;
+// let print = _ => 10;
 
-console.log(print());
+// let print = function (num) {
+//     return num;
+// };
+
+let print = num => num;
+let print2 = (num1 , num2) => num1 + num2;
+
+console.log(print(100));
+console.log(print2(100,50));

08cc1dfd0565b7fb63def510afd54d7f4a3b5318
Mostafa Ouhri <ouhrimostafa@gmail.com>
2025-10-11 20:44:59 +0000

    15.Local Storage

---
+++ 15.Local Storage/111-Document Object Model.js
@@ -0,0 +1,40 @@
+/*
+===> BOM [Browser Object Model]
+- local Storage
+--- setItem
+--- getItem
+--- removeItem
+--- clear
+--- key
+
+===> Info 
+--- No expiration Time 
+--- HTTP And Https 
+--- private Tab
+*/
+console.log("%c===> [BOM] #111 <===", "color:red");
+
+// window.localStorage
+window.localStorage.setItem("color", "red");
+window.localStorage.fontWeight = "bold";
+window.localStorage["fontSize"] = "20px";
+
+console.log(window.localStorage);
+console.log(typeof window.localStorage);
+
+// get 
+console.log(window.localStorage.getItem('color'));
+console.log(window.localStorage.color);
+console.log(window.localStorage["color"]);
+
+// set Color in page 
+document.body.style.backgroundColor = window.localStorage.getItem('color');
+
+// get key
+console.log(window.localStorage.key(0));
+
+// remove one
+window.localStorage.removeItem('fontWeight');
+
+// clear all
+// window.localStorage.clear();
